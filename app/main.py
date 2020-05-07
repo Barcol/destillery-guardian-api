00d949f42808339016be53ac887df212dc4f76b8
Radek Bigaj <r.bigaj@fibcode.com>
2020-05-07 19:04:40 +0200

    hints added

--- app/main.py
+++ app/main.py
@@ -26,18 +26,18 @@ def read_sessions(db: SessionLocal = Depends(get_db)):
 
 
 @app.get("/session/{sess_id}")
-def read_session(sess_id, db: SessionLocal = Depends(get_db)):
+def read_session(sess_id: str, db: SessionLocal = Depends(get_db)):
     session = db.query(models.Session).filter_by(id=sess_id).one()
     return session
 
 
 @app.get("/session/{sess_id}/results")
-def get_result(sess_id, db: SessionLocal = Depends(get_db)):
+def get_result(sess_id: str, db: SessionLocal = Depends(get_db)):
     results = db.query(models.Result).filter_by(session_id=sess_id).all()
     return results
 
 
 @app.get("/result/{res_id}")
-def get_results(res_id, db: SessionLocal = Depends(get_db)):
+def get_results(res_id: str, db: SessionLocal = Depends(get_db)):
     result = db.query(models.Result).filter_by(id=res_id).all()
     return result
