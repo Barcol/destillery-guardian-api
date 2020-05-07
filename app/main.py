ef39d53660396d31f19d33c3d2e80387c957b3b5
Radek Bigaj <r.bigaj@fibcode.com>
2020-05-07 20:33:48 +0200

    PR notes applied

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from fastapi import FastAPI, Depends
 
 from app import models
@@ -15,29 +17,29 @@ def get_db():
 
 
 @app.get("/")
-def read_root(db: SessionLocal = Depends(get_db)):
+def read_root(db: SessionLocal = Depends(get_db)) -> Dict:
     return {"Hello": "World"}
 
 
 @app.get("/sessions")
-def read_sessions(db: SessionLocal = Depends(get_db)):
+def read_sessions(db: SessionLocal = Depends(get_db)) -> List:
     sessions = db.query(models.Session).all()
     return sessions
 
 
 @app.get("/session/{sess_id}")
-def read_session(sess_id: str, db: SessionLocal = Depends(get_db)):
+def read_session(sess_id: str, db: SessionLocal = Depends(get_db)) -> models.Session:
     session = db.query(models.Session).filter_by(id=sess_id).one()
     return session
 
 
 @app.get("/session/{sess_id}/results")
-def get_result(sess_id: str, db: SessionLocal = Depends(get_db)):
+def get_result(sess_id: str, db: SessionLocal = Depends(get_db)) -> List:
     results = db.query(models.Result).filter_by(session_id=sess_id).all()
     return results
 
 
 @app.get("/result/{res_id}")
-def get_results(res_id: str, db: SessionLocal = Depends(get_db)):
+def get_results(res_id: str, db: SessionLocal = Depends(get_db)) -> models.Result:
     result = db.query(models.Result).filter_by(id=res_id).all()
     return result
