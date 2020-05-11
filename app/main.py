f6e4d1e872cb33efe723274557ecc3d10808153d
Radek Bigaj <r.bigaj@fibcode.com>
2020-05-11 21:23:25 +0200

    applied PR notes

--- app/main.py
+++ app/main.py
@@ -17,12 +17,12 @@ def get_db():
 
 
 @app.get("/")
-def read_root(db: SessionLocal = Depends(get_db)) -> Dict:
+def read_root(db: SessionLocal = Depends(get_db)) -> Dict[str, str]:
     return {"Hello": "World"}
 
 
 @app.get("/sessions")
-def read_sessions(db: SessionLocal = Depends(get_db)) -> List:
+def read_sessions(db: SessionLocal = Depends(get_db)) -> List[models.Session]:
     sessions = db.query(models.Session).all()
     return sessions
 
@@ -34,7 +34,7 @@ def read_session(sess_id: str, db: SessionLocal = Depends(get_db)) -> models.Ses
 
 
 @app.get("/session/{sess_id}/results")
-def get_result(sess_id: str, db: SessionLocal = Depends(get_db)) -> List:
+def get_result(sess_id: str, db: SessionLocal = Depends(get_db)) -> List[models.Result]:
     results = db.query(models.Result).filter_by(session_id=sess_id).all()
     return results
 
