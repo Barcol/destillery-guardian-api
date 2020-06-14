968fbbdd356e0965d52d11f2c750b93589d99354
Barcol <36864004+Barcol@users.noreply.github.com>
2020-06-14 17:57:29 +0200

    New column in database; first data saving from API! (#3)
    * new column with temperature result; finally storing data from API in database! <3
    
    * schemas and models rerolled entirely
    
    * return objects made as `Output` versions of themselves
    
    * Tried to send proper OutputResult when posting new result
    
    * i gave up
    
    * database removed from git

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
@@ -24,29 +24,34 @@ def read_root(db: SessionLocal = Depends(get_db)) -> RedirectResponse:
 
 
 @app.get("/sessions")
-def read_sessions(db: SessionLocal = Depends(get_db)) -> List[models.Session]:
+def read_sessions(db: SessionLocal = Depends(get_db)) -> List[schemas.OutputSession]:
     sessions = db.query(models.Session).all()
-    return sessions
+    return [schemas.OutputSession.from_orm(session) for session in sessions]
 
 
 @app.get("/sessions/{sess_id}")
-def read_session(sess_id: str, db: SessionLocal = Depends(get_db)) -> models.Session:
+def read_session(sess_id: str, db: SessionLocal = Depends(get_db)) -> schemas.OutputSession:
     session = db.query(models.Session).filter_by(id=sess_id).one()
-    return session
+    return schemas.OutputSession.from_orm(session)
 
 
 @app.get("/sessions/{sess_id}/results")
-def get_results(sess_id: str, db: SessionLocal = Depends(get_db)) -> List[schemas.Result]:
-    results = db.query(schemas.Result).filter_by(session_id=sess_id).all()
-    return results
+def get_results(sess_id: str, db: SessionLocal = Depends(get_db)) -> List[schemas.OutputResult]:
+    results = db.query(models.Result).filter_by(session_id=sess_id).all()
+    return [schemas.OutputResult.from_orm(result) for result in results]
 
 
 @app.get("/sessions/{sess_id}/results/{res_id}")
-def get_result(res_id: str, sess_id: str, db: SessionLocal = Depends(get_db)) -> schemas.Result:
-    result = db.query(schemas.Result).filter_by(id=res_id).all()
-    return result
+def get_result(res_id: str, sess_id: str, db: SessionLocal = Depends(get_db)) -> schemas.OutputResult:
+    result = db.query(models.Result).filter_by(id=res_id).one()
+    return schemas.OutputResult.from_orm(result)
 
 
-@app.post('/sessions/{sess_id}/results', response_model=schemas.Result)
-async def create_result(sess_id: int, result: schemas.Result, db: SessionLocal = Depends(get_db)):
-    return result.dict()
+@app.post('/sessions/{sess_id}/results')
+async def create_result(sess_id: int,
+                        result: schemas.Result,
+                        db: SessionLocal = Depends(get_db)) -> Dict[str, str]:
+    result_to_database = models.Result(session_id=sess_id, **result.dict())
+    db.add(result_to_database)
+    db.commit()
+    return {"status": "OK"}
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,25 @@
 from pydantic import BaseModel
 
 
+class Session(BaseModel):
+    name: str
+    is_finished: bool
+
+
+class OutputSession(Session):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
 class Result(BaseModel):
     temperature_1: int
+
+
+class OutputResult(Result):
+    id: int
+    session_id: int
+
+    class Config:
+        orm_mode = True
