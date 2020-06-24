08f3a9b0451d9098c8966e39fdcb7a98560110ec
Radek Bigaj <r.bigaj@fibcode.com>
2020-06-24 18:33:27 +0200

    Everything is done as it should be xD

--- app/main.py
+++ app/main.py
@@ -26,19 +26,19 @@ def read_root(db: SessionLocal = Depends(get_db)) -> RedirectResponse:
 @app.get("/sessions")
 def read_sessions(db: SessionLocal = Depends(get_db)) -> List[schemas.OutputSession]:
     sessions = db.query(models.Session).all()
-    return [schemas.OutputSession.from_orm(session) for session in sessions]
+    return sessions
 
 
 @app.get("/sessions/{sess_id}")
 def read_session(sess_id: str, db: SessionLocal = Depends(get_db)) -> schemas.OutputSession:
     session = db.query(models.Session).filter_by(id=sess_id).one()
-    return schemas.OutputSession.from_orm(session)
+    return session
 
 
 @app.get("/sessions/{sess_id}/results")
 def get_results(sess_id: str, db: SessionLocal = Depends(get_db)) -> List[schemas.OutputResult]:
     results = db.query(models.Result).filter_by(session_id=sess_id).all()
-    return [schemas.OutputResult.from_orm(result) for result in results]
+    return results
 
 
 @app.get("/sessions/{sess_id}/results/{res_id}")
@@ -50,25 +50,29 @@ def get_result(res_id: str, sess_id: str, db: SessionLocal = Depends(get_db)) ->
 @app.post('/sessions/{sess_id}/results')
 async def create_result(sess_id: int,
                         result: schemas.Result,
-                        db: SessionLocal = Depends(get_db)) -> Dict[str, str]:
+                        db: SessionLocal = Depends(get_db)) -> schemas.OutputResult:
     result_to_database = models.Result(session_id=sess_id, **result.dict())
     db.add(result_to_database)
     db.commit()
-    return {"status": "OK"}
+    db.refresh(result_to_database)
+    return result_to_database
 
 
 @app.post('/sessions')
-async def create_session(session: schemas.Session, db: SessionLocal = Depends(get_db)) -> Dict[str, str]:
-    db.add(models.Session(**session.dict()))
+async def create_session(session: schemas.Session, db: SessionLocal = Depends(get_db)) -> schemas.OutputSession:
+    session_to_database = models.Session(**session.dict())
+    db.add(session_to_database)
     db.commit()
-    return {"status": "OK"}
+    db.refresh(session_to_database)
+    return session_to_database
 
 
 @app.put('/sessions/{sess_id}/finish')
-async def finish_session(sess_id: int, db: SessionLocal = Depends(get_db)) -> str:
+async def finish_session(sess_id: int, db: SessionLocal = Depends(get_db)) -> schemas.OutputSession:
     session = db.query(models.Session).filter_by(id=sess_id).one()
     if session.is_finished:
         raise HTTPException(status_code=304, detail="This session is already finished")
     session.is_finished = True
     db.commit()
-    return "OK"
+    db.refresh(session)
+    return session
